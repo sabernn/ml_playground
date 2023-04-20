0079a7c71382b4c165fe7e7b8ac9775214462289
sabernn <60275211+sabernn@users.noreply.github.com>
2023-04-20 16:07:44 -0500

    generating random cracks from left edge

--- stat_pg.py
+++ stat_pg.py
@@ -5,28 +5,37 @@ import matplotlib.pyplot as plt
 
 
 def generate_crack():
-    count = np.random.randint(1, 100)
+    img_size = 1024
+    count = np.random.randint(10, 30)
     print(count)
-    img = np.zeros((100, 100))
+    img = np.zeros((img_size, img_size))
     # count = 10*np.random.uniform(0, 1)
-    x = np.linspace(0, 1, 100)
-    y = np.ones(100) + np.random.normal(0, 0.1, 100)
+    x = np.linspace(0, 1, img_size)
+    y = np.linspace(0, 1, img_size)
+    yp = np.random.uniform(0.25*img_size,0.75*img_size)
     output = [count]
     for ind in range(len(img[0])):
-        count = abs(np.random.normal(count-1, 0.5))
+        count = (1-abs(np.random.normal(0, 0.005)))*count
+        y[ind] = yp
+        yp = np.random.normal(0, 1) + yp
         # count - np.round(np.random.uniform(0, 1)*count,0)
         print(count)
         output.append(count)
         if count == 0:
             break
         # ind += 1
-    return output,x,y
+        indy = np.int(y[ind])
+        img[indy-int(count/2):indy+int(count/2),ind] = 1
+
+    return output,x,y,img
 
 
 if __name__ == '__main__':
-    crack,x,y = generate_crack()
+    crack,x,y,img = generate_crack()
     plt.plot(crack)
     plt.show()
     plt.plot(x,y)
+    plt.show()
+    plt.imshow(img*255)
     plt.show()
 
