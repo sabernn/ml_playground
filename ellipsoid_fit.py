932f08f344775a7d06eca47039192a4c458b36c5
sabernn <mnemat2@lsu.edu>
2023-06-02 14:37:06 -0500

    placed an ellipsoid in the center

--- ellipsoid_fit.py
+++ ellipsoid_fit.py
@@ -5,12 +5,34 @@ import matplotlib.pyplot as plt
 
 
 real = np.load('realellipsoid.npy')
+real = real / (2**16)
+
+rmin = 50
+rmax = 2 * rmin
+
+X = real.shape[0]    # pixels
+Y = real.shape[1]    # pixels
+Z = real.shape[2]    # pixels
+
+x_ = np.linspace(0,X-1,X)
+y_ = np.linspace(0,Y-1,Y)
+z_ = np.linspace(0,Z-1,Z)
+
+x, y, z = np.meshgrid(x_, y_, z_, indexing='ij')
+
+xc = X/2
+yc = Y/2
+zc = Z/2
+
+ellipsoid = (x - xc)**2/1**2 + (y - yc)**2/1**2 + (z-zc)**2/2**2<= rmin**2
+ellipsoid = ellipsoid.astype(np.int8)
 
 print(real.shape)
 
-for i in range(16):
-    plt.subplot(4,4,i+1)
-    plt.imshow(real[8*i])
+for i in range(25):
+    plt.subplot(5,5,i+1)
+    plt.imshow(real[5*i], cmap='gray')
+    plt.imshow(ellipsoid[5*i], alpha=0.5)
 # plt.subplot(3,3,1)
 # plt.imshow(real[50])
 
