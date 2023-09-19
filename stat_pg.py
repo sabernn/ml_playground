d153c3d7fd342a724ab80a187394fb33104bb84f
Saber Nemati <mnemat2@lsu.edu>
2023-09-18 19:05:33 -0500

    added vertical symmetry constraint to crack openning

--- stat_pg.py
+++ stat_pg.py
@@ -6,7 +6,7 @@ from scipy.ndimage import gaussian_filter
 import cv2
 
 
-save = True
+save = False
 
 def generate_crack():
     img_size = 1024
@@ -66,27 +66,35 @@ def crack_surface(img_size: int, center: tuple):
     # img_size = 1024
     height = img_size/20
     width = img_size/4
-    count_ul = height
-    count_ur = height
-    count_ll = height
-    count_lr = height
+    count_l = height
+    count_r = height
+    # count_ul = height
+    # count_ur = height
+    # count_ll = height
+    # count_lr = height
     img = np.zeros((img_size, img_size))
     count = 0
     cx = center[0]
     cyl = center[1]
     cyr = center[1]
     smoothness_factor = 1 # higher = smoother
-    while count_ul > 1 and count_ur > 1 and count_ll > 1 and count_lr > 1 and count < img_size/2:
+    # while count_ul > 1 and count_ur > 1 and count_ll > 1 and count_lr > 1 and count < img_size/2:
+    while count_l > 1 and count_r > 1 and count < img_size/2:
         # random asymmetrical decay of crack width
         if count%smoothness_factor == 0:
-            count_ul *= (1-abs(np.random.normal(0, 0.01*smoothness_factor)))
-            count_ur *= (1-abs(np.random.normal(0, 0.01*smoothness_factor)))
-            count_ll *= (1-abs(np.random.normal(0, 0.01*smoothness_factor)))
-            count_lr *= (1-abs(np.random.normal(0, 0.01*smoothness_factor)))
+            count_l *= (1-abs(np.random.normal(0, 0.01*smoothness_factor)))
+            count_r *= (1-abs(np.random.normal(0, 0.01*smoothness_factor)))
+            # count_ul *= (1-abs(np.random.normal(0, 0.01*smoothness_factor)))
+            # count_ur *= (1-abs(np.random.normal(0, 0.01*smoothness_factor)))
+            # count_ll *= (1-abs(np.random.normal(0, 0.01*smoothness_factor)))
+            # count_lr *= (1-abs(np.random.normal(0, 0.01*smoothness_factor)))
             cyl += np.random.normal(0, 1)
             cyr += np.random.normal(0, 1)
-        img[int(cyl-count_ll):int(cyl+count_ul),cx-count:cx-count+1] = 255
-        img[int(cyr-count_lr):int(cyr+count_ur),cx+count:cx+count+1] = 255
+        # img[int(cyl-count_ll):int(cyl+count_ul),cx-count:cx-count+1] = 255
+        # img[int(cyr-count_lr):int(cyr+count_ur),cx+count:cx+count+1] = 255
+
+        img[int(cyl-count_l/2):int(cyl+count_l/2),cx-count:cx-count+1] = 255
+        img[int(cyr-count_r/2):int(cyr+count_r/2),cx+count:cx+count+1] = 255
         count += 1
 
     return img
@@ -102,7 +110,10 @@ def crack_volume(img,z_height=2048, shrink_factor_width=10,shrink_factor_length=
         # print(np.sum(output))
         count += 1
         # output = np.round(gaussian_filter(output, sigma=(shrink_factor_length,shrink_factor_width))/255)*255
-        output = np.round(cv2.erode(output, kernel)/255)*255
+        # output = np.round(cv2.erode(output, kernel)/255)*255
+        output = cv2.erode(output, kernel)
+        # output = cv2.dilate(output, kernel,iterations=1)
+        print(np.sum(output))
         vol[count,:,:] = output
 
     return vol
@@ -113,10 +124,11 @@ if __name__ == '__main__':
     # crack,x,y,img = generate_crack()
     # crack,x,y,z,vol = generate_crack_3d()
 
-    scrack = crack_surface(img_size=128,
-                            center=(64,64))
+    scrack = crack_surface(img_size=1024,
+                            center=(512,512))
     # vcrack = crack_volume(scrack,center=(512,512))
-    vol = crack_volume(scrack,z_height=1024,shrink_factor_width=2,shrink_factor_length=2)
+    # vol = crack_volume(scrack,z_height=2048,shrink_factor_width=np.random.randint(1,2),shrink_factor_length=np.random.randint(1,2))
+    vol = crack_volume(scrack,z_height=2048,shrink_factor_width=2,shrink_factor_length=2)
     # plt.plot(crack)
     # plt.show()
     # plt.plot(x,y)
@@ -133,7 +145,7 @@ if __name__ == '__main__':
     plt.figure(figsize=(10,10))
     for i in range(9):
         plt.subplot(3,3,i+1)
-        plt.imshow(vol[i])
+        plt.imshow(vol[10*i])
 
     plt.show()
 
